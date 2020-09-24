252876540cd18886e60a4fdb5c28b6b287c377e0
wushaolin <15265249516@163.com>
2020-09-24 23:35:36 +0800

    What:  add Topnav.vue
    Why:
    
    *
    
    How:
    
    *

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <Topnav />
     <div class="banner">
       <h1>vvheel UI Framework</h1>
       <h2>This is a awesome UI framework</h2>
@@ -12,32 +9,14 @@
         <a href="">开始</a>
       </p>
     </div>
-
   </div>
 </template>
 
 <script>
+import Topnav from "../components/Topnav.vue";
 export default {
-
-}
+  components: {
+    Topnav,
+  },
+};
 </script>
-
-<style lang="scss" scoped>
-  .topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-}
-</style>
